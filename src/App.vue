99266c504df05448ef4082a745a1795f358ad800
Julien Reboul <julien.reboul@bonitasoft.com>
2017-01-09 00:29:01 +0100

    add vuetify style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app top-navbar>
     <header>
-      <v-navbar></v-navbar>
+      <v-navbar>
+        <v-navbar-logo>Jeann Labourbe</v-navbar-logo>
+        <v-navbar-items v-bind:items="classes"></v-navbar-items>
+      </v-navbar>
     </header>
     <main>
       <v-content>
@@ -16,10 +19,27 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      classes: [{
+        href: '#!',
+        text: 'PS-MS',
+      }, {
+        href: '#!',
+        text: 'GS-CP',
+      }, {
+        href: '#!',
+        text: 'CE1-CE2',
+      }, {
+        href: '#!',
+        text: 'CM1-CM2',
+      }],
+    };
   },
   mounted() {
     this.$vuetify.init();
   },
 };
 </script>
+<style>
+@import '../node_modules/vuetify/dist/vuetify.min.css'
+</style>
